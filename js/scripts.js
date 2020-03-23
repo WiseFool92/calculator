cd96573a2d58a6cab2f32d3511c3f76bb29afedd
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-03-23 14:28:52 -0700

    js calc

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,35 @@
 var add = function(number1, number2) {
-    return number1 + number2;
-  };
-  
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  
-  alert(add(number1, number2));
-  var add = function(number1, number2) {
-    return number1 + number2;
-  };
-  
-  var number1 = parseInt(prompt("Enter a number to add:"));
-  var number2 = parseInt(prompt("Enter another number to add:"));
-  
-  alert(add(number1, number2));
-  
-  var multiply = function(number3, number4) {
-    return number3 * number4;
-  }
-  
-  var number3 = parseInt(prompt("Enter a number to multiply"));
-  var number4 = parseInt(prompt("Enter another number to multiply by:"));
-  
-  alert(multiply(number3, number4));
-  
-  var divide = function(number5, number6) {
-    return number5/number6;
-  }
-  
-  var number5 = parseInt(prompt("Enter a number to divide"));
-  var number6 = parseInt(prompt("Enter another number to divide by:"));
-  
-  alert(divide(number5, number6));
-  
-  var subtract = function(number7, number8) {
-    return number7-number8;
-  }
-  
-  alert(subtract(number7, number8));
-  
-  var number = parseInt(prompt("Enter a number to subtract"));
-  var number4 = parseInt(prompt("Enter another number to subtract by:"));
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number to add:"));
+var number2 = parseInt(prompt("Enter another number to add:"));
+
+alert(add(number1, number2));
+
+var multiply = function(number3, number4) {
+  return number3 * number4;
+}
+
+var number3 = parseInt(prompt("Enter a number to multiply"));
+var number4 = parseInt(prompt("Enter another number to multiply by:"));
+
+alert(multiply(number3, number4));
+
+var divide = function(number5, number6) {
+  return number5/number6;
+}
+
+var number5 = parseInt(prompt("Enter a number to divide"));
+var number6 = parseInt(prompt("Enter another number to divide by:"));
+
+alert(divide(number5, number6));
+
+var subtract = function(number7, number8) {
+  return number7-number8;
+}
+
+var number7 = parseInt(prompt("Enter a number to subtract"));
+var number8 = parseInt(prompt("Enter another number to subtract by:"));
+
+alert(subtract(number7, number8));
